8cbbfd9615eab0539a8e0dcad14a61413c9f07db
jairomora555 <124050519@upq.edu.mx>
2025-09-10 18:40:19 -0600

    Se creo un "Hola mundo"

--- JS/java.js
+++ JS/java.js
@@ -1,4 +1,51 @@
 console.log ("Hola mundo");
 document.writeln("Hola mundo");
 console.error("Mi error customizado");
-console.error("la aplicacion a fallado");
+console.error("la aplicacion ha fallado");
+
+//Tipos de datos 
+"Hello World" //string
+
+10000000 // number 
+-2.3
+
+// boolean 
+true
+false
+
+// array
+
+ ["Jairo, Victor, Fernando"];
+ [1, 2, 3];
+ [true, false, true, false];
+
+ //Objecto
+
+ console.log({
+    "username": 'ryan', 
+    "score" : 70.4, 
+    "hours" :14, 
+    "Profesional": true
+
+ });
+
+ //Variables
+ var name = "Jairo";
+ console.log(name);
+ let name2 = "Victor";
+ console.log(name2);
+ const PI = 3.1416;
+ console.log(PI);
+
+ //Suma de dos números
+ let num1 = 10;
+ let num2 = 20;
+
+ let result = num1 + num2;
+ console.log(result);
+
+ let nameresult = name + name2;
+ console.log(nameresult);
+
+ result = num1 == name2;
+ console.log(result);
